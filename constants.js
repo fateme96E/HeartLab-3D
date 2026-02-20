d83fd9cb8fc5da61f3259b459db7fc13e2882f38
Fateme Esmaili <92683438+fateme96E@users.noreply.github.com>
2026-02-20 20:45:00 +0330

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,111 @@
+export const Messages = {
+
+    'apex': {
+        title: 'Apex',
+
+        descriptions: [
+            'The lowest and pointed tip of the heart.',
+            'Most of it is formed by the left ventricle.',
+            'It plays a contractile role in the heartbeat.',
+        ],
+
+        bloodWay: ['Part of the left ventricle']
+    },
+
+    'aorta': {
+        title: 'Aorta',
+
+        descriptions: [
+            'The largest artery in the body.',
+            'It exits the left ventricle and gives rise to three main branches in the aortic arch.',
+            'It carries oxygen-rich blood from the left ventricle to various organs.',
+        ],
+
+        bloodWay: ['Left ventricle', 'Aorta', 'Head, arms, trunk, legs']
+    },
+
+    'superior_vena_cava': {
+        title: 'Superior Vena Cava',
+
+        descriptions: [
+            'A large vein that returns deoxygenated blood from the upper half of the body to the right atrium.',
+        ],
+
+       bloodWay: ['Head and arms', 'Superior vena cava', 'Right atrium']
+    },
+
+    'pulmonary_trunk': {
+        title: 'Pulmonary Trunk',
+
+        descriptions: [
+            'An artery that exits the right ventricle and divides into left and right branches.',
+            'It carries blood to the lungs.'
+        ],
+
+       bloodWay: ['Right ventricle', 'Pulmonary trunk', 'Pulmonary branches', 'Lungs']
+    },
+
+    'left_pulmonary_artery': {
+        title: 'Left Pulmonary Artery',
+
+        descriptions: [
+            'A branch of the pulmonary trunk that carries blood to the left lung.'
+        ],
+
+       bloodWay: ['Pulmonary trunk', 'Left pulmonary artery', 'Left lung']
+    },
+
+    'right_pulmonary_artery': {
+        title: 'Right Pulmonary Artery',
+
+        descriptions: [
+            'A branch of the pulmonary trunk that carries blood to the right lung.'
+        ],
+
+       bloodWay: ['Pulmonary trunk', 'Right pulmonary artery', 'Right lung']
+    },
+
+    'left_atrium': {
+        title: 'Left Atrium',
+
+        descriptions: [
+            'Receives oxygen-rich blood from the lungs.',
+            'Blood enters this chamber through the pulmonary veins and then moves into the left ventricle.'
+        ],
+
+       bloodWay: ['Lungs', 'Pulmonary veins', 'Left atrium', 'Left ventricle']
+    },
+
+    'right_atrium': {
+        title: 'Right Atrium',
+
+        descriptions: [
+            'Receives deoxygenated blood from the entire body.',
+            'Blood enters this chamber through the superior and inferior vena cava and then flows into the right ventricle.'
+        ],
+
+       bloodWay: ['Body', 'Vena Cava', 'Right atrium', 'Right ventricle']
+    },
+
+    'left_ventricle': {
+        title: 'Left Ventricle',
+
+        descriptions: [
+            'The strongest chamber of the heart.',
+            'It has the thickest heart wall.',
+            'It pumps oxygen-rich blood at high pressure through the aorta to the entire body.'
+        ],
+
+       bloodWay: ['Left atrium', 'Left ventricle', 'Aorta', 'Entire body']
+    },
+
+    'right_ventricle': {
+        title: 'Right Ventricle',
+
+        descriptions: [
+            'It pumps deoxygenated blood to the lungs so that gas exchange can occur.'
+        ],
+
+       bloodWay: ['Right atrium', 'Right ventricle', 'Pulmonary trunk', 'Lungs']
+    },
+}
---
+++ script.js
@@ -0,0 +1,139 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { getMouseVector2, checkRayIntersections } from './helpers/RayCastHelper';
+import { getMeshColor } from './helpers/ColorsHelper';
+import { Messages } from './constants';
+import { createLabelRenderer, HideLabel, showLabel } from './helpers/LabelBox';
+
+
+const HOVER_INTENSITY = 0.3;
+const CLICK_INTENSITY = 0.8;
+
+const interactiveMeshes = [
+    'aorta',
+    'apex',
+    'right_atrium',
+    'left_atrium',
+    'right_ventricle',
+    'left_ventricle',    
+    'superior_vena_cava',
+    'pulmonary_trunk', 
+    'right_pulmonary_artery',
+    'left_pulmonary_artery',    
+];
+
+
+let hoveredMesh = null;
+let selectedMesh = null;
+let currentLabel = null; 
+const selectedMeshRef = { current: selectedMesh };
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const rayCaster = new THREE.Raycaster();
+let mousePointer = new THREE.Vector2();
+scene.background = new THREE.Color(0xE3E3E3);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const labelRenderer = createLabelRenderer();
+
+let heartMeshes = [];
+const loader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
+const gltf = await loader.loadAsync( 'heart.glb' );
+
+gltf.scene.traverse((child) => {
+    if (child.isMesh) heartMeshes.push(child)
+});
+scene.add( gltf.scene );
+
+
+const ambientLight = new THREE.AmbientLight()
+ambientLight.color = new THREE.Color(0xffffff)
+ambientLight.intensity = 1
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xF29683, 0.9)
+scene.add(directionalLight)
+directionalLight.position.set(1, 0.25, 0)
+
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set( 0, 0, 2 );
+controls.enableDamping = true;
+controls.update();
+
+
+function onClick(event) {
+    mousePointer = getMouseVector2(event, renderer);
+    const intersect = checkRayIntersections(mousePointer, camera, rayCaster, heartMeshes);
+
+    if (intersect) {
+        const intersectedObject = intersect.object; 
+
+        if (selectedMeshRef.current !== intersectedObject) {
+            if (selectedMeshRef.current) {
+                selectedMeshRef.current.material.emissive.set(0x000000);
+            }
+
+            selectedMeshRef.current = intersectedObject;
+            selectedMeshRef.current.material.emissive.set(getMeshColor(intersectedObject.name));
+            selectedMeshRef.current.material.emissiveIntensity = CLICK_INTENSITY;
+
+            if (interactiveMeshes.includes(intersectedObject.name)) {
+                currentLabel = showLabel(scene, intersect, currentLabel, Messages, selectedMeshRef);
+            } else {
+                currentLabel = HideLabel(currentLabel);
+            }
+        }
+    }
+}
+
+window.addEventListener('click', onClick);
+
+document.addEventListener('mousemove', onMouseMove, false)
+
+function onMouseMove(event) {
+    camera.updateMatrixWorld();
+    mousePointer = getMouseVector2(event, renderer);
+    const intersections = checkRayIntersections(mousePointer, camera, rayCaster, heartMeshes);
+
+    if (intersections) {
+        const intersected = intersections.object;
+
+        if (hoveredMesh !== intersected) {
+            if (hoveredMesh && hoveredMesh !== selectedMeshRef.current) {
+                hoveredMesh.material.emissive.set(0x000000);
+            }
+
+            hoveredMesh = intersected;
+            document.body.style.cursor = interactiveMeshes.includes(hoveredMesh.name) ? 'pointer' : 'default';
+            if (hoveredMesh !== selectedMeshRef.current) {
+                hoveredMesh.material.emissive.set(getMeshColor(hoveredMesh.name));
+                hoveredMesh.material.emissiveIntensity = HOVER_INTENSITY;
+            }
+        }
+    } else {
+        if (hoveredMesh && hoveredMesh !== selectedMeshRef.current) {
+            hoveredMesh.material.emissive.set(0x000000);
+            hoveredMesh = null;
+            document.body.style.cursor = 'default';
+        }
+    }
+}
+
+function animate() {
+    controls.update();
+
+
+    renderer.render(scene, camera);  
+    labelRenderer.render(scene, camera);    
+
+}
+renderer.setAnimationLoop( animate );
